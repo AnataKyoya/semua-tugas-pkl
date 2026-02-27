2f90f461139db75d12101ad81731884ef068e9bf
AnataKyoya <tzkinggamingmoba@gmail.com>
2026-02-27 07:30:35 +0700

    model database scraper web

--- app/Models/ScrapedResult.php
+++ app/Models/ScrapedResult.php
@@ -4,28 +4,26 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class BMCUserModel extends Model
+class ScrapedResult extends Model
 {
-    protected $table            = 'tb_bmc_users';
+    protected $table            = 'tb_scraped_result';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = ['username', 'password', 'email'];
+    protected $allowedFields    = ['data', 'status', 'website'];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
 
     protected array $casts = [];
-    protected array $castHandlers = [];
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
 
     // Validation
     protected $validationRules      = [];
